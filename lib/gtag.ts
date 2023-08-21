ca3f3ddc5b4cbb3acc29869d15c62b9df4429817
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-08-21 18:42:54 -0400

    [fix}: hopefully on gtag

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -2,10 +2,10 @@ import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useRef } from "react";
 
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
-
+//TODO: switched out window.gtag "as string" instead of || ''
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
-  window.gtag("config", GA_TRACKING_ID as string, {
+  window.gtag("config", GA_TRACKING_ID || "", {
     page_path: url,
   });
 };
