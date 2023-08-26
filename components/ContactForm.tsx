54d8e9081047e98e6f4fc02eeb46da4cdf236c3d
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-08-26 00:10:13 -0400

    [feat]: form submit message component pt2

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -119,21 +119,6 @@ const ContactForm = () => {
           isModalOpen={isModalOpen}
           setIsModalOpen={setIsModalOpen}
         />
-        // <div
-        //   className={`w-[280px] h-[100px] lg:h-1/2 lg:w-1/2 rounded-lg flex justify-center items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 ${
-        //     isError
-        //       ? "bg-red-100 border-red-500 text-red-700"
-        //       : "border-green-500 text-green-700 bg-green-100"
-        //   }`}
-        //   role="alert"
-        // >
-        //   <X
-        //     size={26}
-        //     className="text-dark cursor-pointer absolute top-2 right-2"
-        //     onClick={closeModal}
-        //   />
-        //   <p className="font-bold uppercase text-base">{modalContent}</p>
-        // </div>
       )}
     </>
   );
