1fa930a550dc1641c05a55ff8a07decd6c7f7f20
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-11-27 23:07:40 -0500

    [feat]: upgrading next to 14

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
-    domains: ["boenonnbcwyqchdfeiil.supabase.co"],
+    remotePatterns: [
+      {
+        hostname: "boenonnbcwyqchdfeiil.supabase.co",
+      },
+    ],
   },
   async redirects() {
     return [
