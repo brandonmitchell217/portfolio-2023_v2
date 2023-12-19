e9543e0fff0c7c0852650e77f41693c2b69d2e43
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-12-18 23:19:32 -0500

    [wip]: blurDataUrl

--- lib/util.tsx
+++ lib/util.tsx
@@ -65,3 +65,20 @@ export const NavigationLinks: NavigationLinksProps[] = [
 export function scrollToTop() {
   window.scrollTo(0, 0);
 }
+
+export function getBase64FromUrl1(url: any) {
+  return `data:image/png;base64,${url}`;
+}
+
+export const getBase64FromUrl = async (url: any) => {
+  const data = await fetch(url);
+  const blob = await data.blob();
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onloadend = () => {
+      const base64data = reader.result;
+      resolve(base64data);
+    };
+  });
+};
