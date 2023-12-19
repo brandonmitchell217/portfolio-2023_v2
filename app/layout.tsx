e189b4c44d3391bee25ef762fc743189c26ab731
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-12-18 23:26:00 -0500

    added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/ui/Footer";
 import MobileHeader from "@/components/ui/MobileHeader";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const outfit = Outfit({
   weight: ["300", "400", "500", "600", "700", "800", "900"],
@@ -40,6 +41,7 @@ export default function RootLayout({
           <Footer />
           <GoogleAnalytics />
           <Analytics />
+          <SpeedInsights />
         </body>
       </html>
     </>
