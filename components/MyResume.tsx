afbfa430bfd420e0da54823b16f1a22622cec77d
Brandon Mitchell <brandonmitchell217@gmail.com>
2024-01-29 12:10:19 -0500

    [feat]: updated resume

--- components/MyResume.tsx
+++ components/MyResume.tsx
@@ -25,7 +25,7 @@ const maxWidth = 800;
 type PDFFile = string | File | null;
 
 export default function MyResume() {
-  const [file, setFile] = useState<PDFFile>("../BrandonMitchell-resume.pdf");
+  const [file, setFile] = useState<PDFFile>("../BrandonMitchell-resume1.pdf");
   const [numPages, setNumPages] = useState<number>();
   const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
   const [containerWidth, setContainerWidth] = useState<number>();
